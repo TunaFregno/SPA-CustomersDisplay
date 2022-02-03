88d0ca7fb4595331dcc94a7708295f960e04b19a
Valentina Sulbaran <vjss05@gmail.com>
2022-02-03 16:55:44 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+
+    axios.get('http://myjson.dit.upm.es/api/bins/bw9t')
+      .then((res) => {
+        console.log(res);
+        setCustomers(res.data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  return (
+    console.log('in the customers', customers),
+    <div className="App">
+      <header className="App-header">
+       <p>Hello</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
